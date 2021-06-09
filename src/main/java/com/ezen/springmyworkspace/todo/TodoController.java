ab861f5e48f4ea3e8d3f55304175811072c68661
Jonguk Baek <bju9710@gmail.com>
2021-06-09 17:52:05 +0900

    Todo,Contact Paging

--- src/main/java/com/ezen/springmyworkspace/todo/TodoController.java
+++ src/main/java/com/ezen/springmyworkspace/todo/TodoController.java
@@ -7,12 +7,16 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,7 +31,12 @@ public class TodoController {
 
 	@GetMapping(value = "/todos")
 	public List<Todo> getTodoList() {
-		return repo.findAll();
+		return repo.findAll(Sort.by("id").descending());
+	}
+
+	@GetMapping(value = "/todos/paging")
+	public Page<Todo> getTodoListPaging(@RequestParam int page, @RequestParam int size) {
+		return repo.findAll(PageRequest.of(page, size, Sort.by("id").descending()));
 	}
 
 	@PostMapping(value = "/todos")
